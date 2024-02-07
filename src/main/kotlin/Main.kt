6e70b05387adae776c6edb47fb086f3d996b1d8d
student <mwendezmasaku@gmail.com>
2024-02-07 22:54:51 +0300

    kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    var name = "susanmwende"
+    println(name)
+    var age: Byte = 24
+    println(age)
+    var phoneNumber = "0707071167"
+    println(phoneNumber)
+    var weightInKg: Float =55.5F
+    println(weightInKg)
+    var kenyan: Boolean = true
+    println(kenyan)
+    var nonkenyan : Boolean = false
+    println(nonkenyan)
+
+}
+
+
+
